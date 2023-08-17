45050630236adea5eed6248fef2ca569b7cddcfb
imViniciuuss <imviniciuuss@gmail.com>
2023-08-17 09:25:09 -0300

    :sparkles: added profile page

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// import { ProductsContext } from '../../contexts/ProductsContext';
+
+import { Container } from './styles';
+
+const Profile: React.FC = () => {
+	// const { products } = useContext(ProductsContext);
+
+	// console.log(products);
+
+	return (
+		<Container>
+			<h1>Coming Soon....</h1>
+		</Container>
+	);
+};
+
+export default Profile;
---
+++ src/pages/Profile/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	display: flex;
+	width: calc(100% - 25%);
+	padding: 20px;
+
+	h1 {
+		color: white;
+	}
+`;
